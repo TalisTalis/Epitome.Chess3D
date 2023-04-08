f6be75132afdbb9d95b52c9e71931fe1158fcaa6
Stanislav <tsarst@gmail.com>
2023-04-08 11:30:05 +0300

    Initial commit

---
+++ Assets/Scripts/ChessPieces/Pawn.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : ChessPiece
+{
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+
+        int direction = (team == 0) ? 1 : -1;
+        
+        //Debug.Log(currentX + " " + (currentY + direction));
+
+        // One in front
+        if (currentY + direction < tileCountY && currentY + direction >= 0)
+        {
+            if (board[currentX, currentY + direction] == null)
+            {
+                //Debug.Log(currentX + " " + currentY);
+                r.Add(new Vector2Int(currentX, currentY + direction));
+            }
+
+            // Two in front
+            if (board[currentX, currentY + direction] == null)
+            {
+                if (team == 0 && currentY == 1 && board[currentX, currentY + (direction * 2)] == null)
+                {
+                    r.Add(new Vector2Int(currentX, currentY + (direction * 2)));
+                }
+                if (team == 1 && currentY == 6 && board[currentX, currentY + (direction * 2)] == null)
+                {
+                    r.Add(new Vector2Int(currentX, currentY + (direction * 2)));
+                }
+            }
+
+            // Kill move
+            if (currentX != tileCountX - 1)
+            {
+                if (board[currentX + 1, currentY + direction] != null && board[currentX + 1, currentY + direction].team != team)
+                {
+                    r.Add(new Vector2Int(currentX + 1, currentY + direction));
+                }
+            }
+            if (currentX != 0)
+            {
+                if (board[currentX - 1, currentY + direction] != null && board[currentX - 1, currentY + direction].team != team)
+                {
+                    r.Add(new Vector2Int(currentX - 1, currentY + direction));
+                }
+            }
+        }
+
+        return r;
+    }
+
+    public override SpecialMove GetSpecialMoves(ref ChessPiece[,] board, ref List<Vector2Int[]> moveList, ref List<Vector2Int> availableMoves)
+    {
+        int direction = (team == 0) ? 1 : -1;
+
+        if ((team == 0 && currentY == 6) || (team == 1 && currentY == 1))
+        {
+            return SpecialMove.Promotion;
+        }
+
+        // En Passant
+        if (moveList.Count > 0)
+        {
+            Vector2Int[] lastMove = moveList[moveList.Count - 1];
+
+            if (board[lastMove[1].x, lastMove[1].y].type == ChessPieceType.Pawn) // If the last piece moved was a pawn
+            {
+                if (Mathf.Abs(lastMove[0].y - lastMove[1].y) == 2) // If the last move was a +2 either direction
+                {
+                    if (board[lastMove[1].x, lastMove[1].y].team != team) // If the move was from the other team
+                    {
+                        if (lastMove[1].y == currentY) // If both pawns are on the same Y
+                        {
+                            if (lastMove[1].x == currentX - 1) // Landed left
+                            {
+                                availableMoves.Add(new Vector2Int(currentX - 1, currentY + direction));
+                                return SpecialMove.EnPassant;
+                            }
+                            if (lastMove[1].x == currentX + 1) // Landed right
+                            {
+                                availableMoves.Add(new Vector2Int(currentX + 1, currentY + direction));
+                                return SpecialMove.EnPassant;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return SpecialMove.None;
+    }
+}
---
+++ Assets/Scripts/Net/NetMessage/NetWelcome.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Networking.Transport;
+
+class NetWelcome : NetMessage
+{
+    public int AssignedTeam { get; set; }
+
+    public NetWelcome()
+    {
+        Code = OpCode.WELCOME;
+    }
+
+    public NetWelcome(DataStreamReader reader)
+    {
+        Code = OpCode.WELCOME;
+        Deserialize(reader);
+    }
+
+    public override void Serialize(ref DataStreamWriter writer)
+    {
+        writer.WriteByte((byte)Code);
+        writer.WriteInt(AssignedTeam);
+    }
+
+    public override void Deserialize(DataStreamReader reader)
+    {
+        // We already read the byte in the NetUtility::OnData
+        AssignedTeam = reader.ReadInt();
+    }
+
+    public override void ReceivedOnClient()
+    {
+        NetUtility.C_WELCOME?.Invoke(this);
+    }
+
+    public override void ReceivedOnServer(NetworkConnection cnn)
+    {
+        NetUtility.S_WELCOME?.Invoke(this, cnn);
+    }
+}
